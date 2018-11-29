d1cdf6a80ec0f067879bc423350dab9f0efea691
BossHsieh <librablue1985@gmail.com>
2018-11-29 17:54:54 +0800

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCh6D730yM-nvPTmc9qqRk63EZhrjJjeKM",
+    authDomain: "vuefs-prod-68697.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-68697.firebaseio.com",
+    projectId: "vuefs-prod-68697",
+    storageBucket: "vuefs-prod-68697.appspot.com",
+    messagingSenderId: "1007197569481"
+}
