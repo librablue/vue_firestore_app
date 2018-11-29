aeffd90ac9d4777fb2546adc03667603fdab9325
librablue <librablue1985@gmail.com>
2018-11-29 22:12:47 +0800

    part 5

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,18 @@
 <template>
     <div id="dashboard">
-        <h3>Dashboard</h3>
+        <ul class="collection with-header">
+            <li class="collection-header">
+                <h4>Employees</h4>
+            </li>
+            <li v-for="employee in employees" :key="employee.id" class="collection-item">
+                <div class="chip">{{employee.dept}}</div>
+                {{employee.employee_id}}:{{employee.name}}
+
+                <router-link class="secondary-content" :to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+                    <i class="fa fa-eye"></i>
+                </router-link>
+            </li>
+        </ul>
         <div class="fixed-action-btn">
             <router-link to="/new" class="btn-floating btn-large red">
                 <i class="fa fa-plus"></i>
@@ -10,12 +22,28 @@
 </template>
 
 <script>
+import db from './firebaseInit';
 export default {
     name: 'dashboard',
     data() {
         return {
-            
+            employees: []
         }
+    },
+    mounted() {
+        db.collection('employees').orderBy('dept').get().then((query) => {
+            query.forEach(doc => {
+                const data = {
+                    id: doc.id,
+                    employee_id: doc.data().employee_id,
+                    name: doc.data().name,
+                    dept: doc.data().dept,
+                    position: doc.data().position
+                };
+
+                this.employees.push(data);
+            });
+        });
     }
 }
 </script>
